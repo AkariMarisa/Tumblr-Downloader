c28ff267ce39e1ef14d3220e18831554af0a15df
Akari Marisa <826436816d@gmail.com>
2026-08-28 23:57:40 +0800

    chore: bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.akarimarisa.tumblrdownloader"
         minSdk = 30
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
     }
 
     signingConfigs {
